7c31c8694db172af140a3d8a7214bada8afc3997
Anders Herbjørnsen <anders.herbjornsen@protonmail.com>
2025-11-02 19:30:02 +0100

    fixed syntax errors

--- custom_components/sas_award_finder/__init__.py
+++ custom_components/sas_award_finder/__init__.py
@@ -7,12 +7,12 @@ from .const import DOMAIN
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up SAS Award Finder from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, ["sensor"])
     return unload_ok
